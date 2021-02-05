4819eb7f8485bf694a24426b930bd7ad00fb09d5
purushothaman-source <purushoth99jun@gmail.com>
2021-02-05 17:40:53 +0530

    ADDED : functionality to calculate Daily EmployeeWage

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -1,10 +1,14 @@
 console.log('Welcome to EmployeeWageComputation !');
 var isPresent = 1;
+empRatePerHr = 20;
 var empCheck = Math.floor(Math.random() * 2); 
-console.log(empCheck);
 if (empCheck == isPresent){
     console.log("Employee is present");
+    empHrs = 8;
 }
 else{
     console.log("Employee is absent");
+    empHrs = 0;
 }
+var salary = empRatePerHr * empHrs ;
+console.log("Salary = ",salary)
