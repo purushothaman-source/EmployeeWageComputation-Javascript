6b7f271674dc4eab4bf017c5a26add9aa0f963db
purushothaman-source <purushoth99jun@gmail.com>
2021-02-05 21:26:38 +0530

    REFRACTORED : refracted to class method for get workHrs

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -1,28 +1,31 @@
-console.log('Welcome to EmployeeWageComputation !');
-var isPresent = 1;
-var isPartTime =2;
+class Employee {
+
+    getWorkHrs(totalWorkingDays, MaxEmpHrs) {
+        var workingDays = 0;
+        var totalEmpHrs = 0;
+        while (workingDays < totalWorkingDays && totalEmpHrs <= MaxEmpHrs) {
+            var isPresent = 1;
+            var isPartTime = 2;
+            var empHrs = 0;
+            var empCheck = Math.floor(Math.random() * 3);
+            workingDays++;
+            switch (empCheck) {
+                case isPresent:
+                    empHrs = 8;
+                    break;
+                case isPartTime:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            totalEmpHrs += empHrs;
+        }
+        return totalEmpHrs;
+    }
+}
 empRatePerHr = 20;
-var MaxEmpHrs = 100;
-var totalWorkingDays = 20;
-workingDays = 0;
-totalEmpHrs =0;
-while (workingDays < totalWorkingDays && totalEmpHrs <= MaxEmpHrs) {
-var empCheck = Math.floor(Math.random() * 3);
-workingDays++; 
-switch(empCheck){
- case isPresent :
-    console.log("Employee is present");
-    empHrs = 8;
-    break;
- case isPartTime :
-    console.log("Employee is part Time");
-    empHrs = 4;
-    break;
- default :   
-    console.log("Employee is absent");
-    empHrs = 0;
-}
- totalEmpHrs += empHrs;
-}
-var salary = empRatePerHr * totalEmpHrs ;
-console.log("Salary = ",salary)
+const employeeWage = new Employee();
+var wage = employeeWage.getWorkHrs(20, 100);
+var salary = empRatePerHr * wage;
+console.log("Salary = ", salary)
