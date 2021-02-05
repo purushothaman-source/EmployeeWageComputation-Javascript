4275b75ea5e6029f59d1a3f340ecd1512b0348f3
purushothaman-source <purushoth99jun@gmail.com>
2021-02-05 17:31:17 +0530

    ADDED : Code to check Employee Attendance

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -1,1 +1,10 @@
 console.log('Welcome to EmployeeWageComputation !');
+var isPresent = 1;
+var empCheck = Math.floor(Math.random() * 2); 
+console.log(empCheck);
+if (empCheck == isPresent){
+    console.log("Employee is present");
+}
+else{
+    console.log("Employee is absent");
+}
