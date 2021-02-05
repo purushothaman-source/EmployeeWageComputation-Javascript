4f0c2730422177c3f084baf001550f799c544834
purushothaman-source <purushoth99jun@gmail.com>
2021-02-05 18:21:44 +0530

    ADDED : Code to add part time

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -1,10 +1,15 @@
 console.log('Welcome to EmployeeWageComputation !');
 var isPresent = 1;
+var isPartTime =2;
 empRatePerHr = 20;
-var empCheck = Math.floor(Math.random() * 2); 
+var empCheck = Math.floor(Math.random() * 3); 
 if (empCheck == isPresent){
     console.log("Employee is present");
     empHrs = 8;
+}
+else if (empCheck == isPartTime){
+    console.log("Employee is part Time");
+    empHrs = 4;
 }
 else{
     console.log("Employee is absent");
