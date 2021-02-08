ebbdc049ac1a1a6b28e4900b7699991b1158f0fc
purushothaman-source <purushoth99jun@gmail.com>
2021-02-08 07:59:14 +0530

    ADDED : Functionality to store DailyWage in array

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -1,30 +1,51 @@
 class Employee {
+    constructor() {
+        this.workingDays = 0;
+        this.totalEmpHrs = 0;
+        this.isPresent = 1;
+        this.isPartTime = 2;
+        this.empHrs = 0;
+        this.dailywage =[];
+        this.i=0;
+    }
 
-    getWorkHrs(company, totalWorkingDays, MaxEmpHrs, empRatePerHr) {
-        var workingDays = 0;
-        var totalEmpHrs = 0;
-        while (workingDays < totalWorkingDays && totalEmpHrs <= MaxEmpHrs) {
-            var isPresent = 1;
-            var isPartTime = 2;
-            var empHrs = 0;
-            var empCheck = Math.floor(Math.random() * 3);
-            workingDays++;
-            switch (empCheck) {
-                case isPresent:
-                    empHrs = 8;
-                    break;
-                case isPartTime:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-            }
-            totalEmpHrs += empHrs;
+    getEmpTotalWage(company, totalWorkingDays, MaxEmpHrs, empRatePerHr) {
+        while (this.workingDays < totalWorkingDays && this.totalEmpHrs <= MaxEmpHrs) {
+            var empCheck = Math.floor(Math.random() * 10) % 3;
+            var empHour = this.getEmpHrs(empCheck);
+            this.workingDays++;
+            this.totalEmpHrs += empHour;
         }
-        var salary = empRatePerHr * totalEmpHrs;
-        console.log("Company = ",company," Salary = ", salary)
+        var salary = empRatePerHr * this.totalEmpHrs;
+        console.log("Company = ", company, " EmpWage = ", salary)
+    }
+
+    getEmpHrs(empCheck) {
+        switch (empCheck) {
+            case this.isPresent:
+                this.empHrs = 8;
+                break;
+            case this.isPartTime:
+                this.empHrs = 4;
+                break;
+            default:
+                this.empHrs = 0;
+        }
+        this.storeinarr(this.empHrs,this.workingDays);
+        return this.empHrs;
+    }
+   
+    storeinarr(emphrs,workingDays){
+         
+        this.dailywage[this.i] = emphrs * 20;
+        this.i++;
     }
 }
 const employeeWage = new Employee();
-employeeWage.getWorkHrs("Dmart",20, 100,20);
-employeeWage.getWorkHrs("Accenture",10, 50,10);
+employeeWage.getEmpTotalWage("Dmart", 20, 100, 20);
+console.log(employeeWage.dailywage);
+const employeeWage1 = new Employee();
+employeeWage1.getEmpTotalWage("Accenture", 10, 50, 10);
+console.log(employeeWage1.dailywage);
+
+
